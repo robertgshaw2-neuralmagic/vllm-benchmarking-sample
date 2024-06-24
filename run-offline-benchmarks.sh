236f5799874497468f583907a2f1e8b0369203ad
Robert Shaw <rshaw@neuralmagic.com>
2024-06-24 01:31:50 +0000

    added run script

---
+++ run-offline-benchmarks.sh
@@ -0,0 +1,13 @@
+MODELS=("meta-llama/Meta-Llama-3-8B-Instruct" "nm-testing/Meta-Llama-3-8B-Instruct-GPTQ" "nm-testing/Meta-Llama-3-8B-Instruct-W8A8-Dyn-Per-Token")
+MAX_NUM_SEQS=256
+
+for model in ${MODELS[@]}; do
+    echo "\n\n"
+    echo "* ==================================================================================== *"
+
+    echo "* === RUNNING ${MODEL} WITH MAX_NUM_SEQS=${MAX_NUM_SEQS}"
+    python benchmark/benchmark_offline.py --model ${MODEL} --max-num-seqs 256
+    
+    echo "* ==================================================================================== *"
+    echo "\n\n"
+done
